a2c4190d70bb21b4f06d28d44c003ec961706dc9
Harshil <harshilsitapara4@gmail.com>
2023-12-07 17:34:27 +0530

    implemented update and delete API

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,21 +1,59 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
+import { createBookDto } from './dto/Create-book.dto';
+import { updateBookDto } from './dto/update-book.dto';
 
 @Controller('book')
 export class BookController {
   constructor(private bookService: BookService) {}
 
+  // Get all books
   @Get()
   async getAllBooks(): Promise<Book[]> {
     return this.bookService.findAll();
   }
 
+  // create new book
   @Post('new')
-  async createBook(
-    @Body()
-    book,
-  ): Promise<Book> {
+  async createBook(@Body() book: createBookDto): Promise<Book> {
     return this.bookService.create(book);
+  }
+
+  // get Book by id
+  @Get(':id')
+  async getBookById(
+    @Param('id')
+    id: string,
+  ): Promise<Book> {
+    return this.bookService.findById(id);
+  }
+
+  // udpate Book by id
+  @Put(':id')
+  async updateBook(
+    @Param('id')
+    id: string,
+    @Body()
+    book: updateBookDto,
+  ): Promise<Book> {
+    return this.bookService.updateBookById(id, book);
+  }
+
+  // Delete book
+  @Delete(':id')
+  async deleteBookById(
+    @Param('id')
+    id: string,
+  ): Promise<Book> {
+    return this.bookService.deleteBookById(id)
   }
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, Body } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { Book } from './schemas/book.schema';
@@ -10,13 +10,37 @@ export class BookService {
     private bookModel: mongoose.Model<Book>,
   ) {}
 
+  //Find all books
   async findAll(): Promise<Book[]> {
     const books = await this.bookModel.find();
     return books;
   }
 
+  // create new book
   async create(book: Book): Promise<Book> {
     const res = await this.bookModel.create(book);
     return res;
+  }
+
+  // Find book by Id
+  async findById(id: string): Promise<Book> {
+    const book = await this.bookModel.findById(id);
+    if (!book) {
+      throw new NotFoundException('Book not found.');
+    }
+    return book;
+  }
+
+  // Update book by Id
+  async updateBookById(id: string, book: Book): Promise<Book> {
+    return await this.bookModel.findByIdAndUpdate(id, book, {
+      new: true,
+      runValidators: true,
+    });
+  }
+
+  // Delete book
+  async deleteBookById(id: string): Promise<Book> {
+    return await this.bookModel.findByIdAndDelete(id, {});
   }
 }
