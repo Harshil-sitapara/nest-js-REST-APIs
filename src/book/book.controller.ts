3f18c8bf18a30678bd1b91e11e7066fb4b7f123e
Harshil <harshilsitapara4@gmail.com>
2023-12-07 15:28:04 +0530

    first commit

---
+++ src/book/book.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schema';
+
+@Controller('book')
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Get()
+  async getAllBooks(): Promise<Book[]> {
+    return this.bookService.findAll();
+  }
+
+  @Post('new')
+  async createBook(
+    @Body()
+    book,
+  ): Promise<Book> {
+    return this.bookService.create(book);
+  }
+}
